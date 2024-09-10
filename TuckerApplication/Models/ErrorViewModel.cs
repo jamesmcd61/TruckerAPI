7a040f953fb4e78b98d18e97e48ca48b17565d87
jamesmcd61 <bezzinga3@gmail.com>
2024-09-10 19:32:15 +0200

    Creation of applications for API and application

---
+++ TuckerApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TuckerApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
